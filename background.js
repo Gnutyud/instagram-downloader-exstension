64bc325a869636e7fad158694f881db8ed6304fc
Gnutyud <nguyenduytungtd3@gmail.com>
2022-01-04 10:50:43 +0700

    add style for popup pages

--- background.js
+++ background.js
@@ -3,7 +3,19 @@ chrome.contextMenus.create({
   id: 'instagram-downloader',
   title: "Instagram downloader",
   contexts: ["all"],
-  "documentUrlPatterns":["https://www.instagram.com/","https://www.instagram.com/*"]
+  "documentUrlPatterns": ["https://www.instagram.com/", "https://www.instagram.com/*"]
+}, () => chrome.runtime.lastError);
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  chrome.tabs.sendMessage(
+    tab.id,
+    "getClickedEl",
+    { frameId: info.frameId },
+    (data) => {
+      let imageSrc = data.value;
+      console.log(imageSrc);
+    },
+  );
 });
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
@@ -12,18 +24,7 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
     let isInstagram = url.includes("https://www.instagram.com/");
     if (isInstagram) {
       instagramTools(activeInfo.tabId);
-    } 
-    // else {
-    //   chrome.action.setIcon({
-    //     path: {
-    //       "16" : "./icons/icon16.png",
-    //       "32" : "./icons/icon32.png",
-    //       "48" : "./icons/icon48.png",
-    //       "128" : "./icons/icon128.png"
-    //     },
-    //     tabId: activeInfo.tabId,
-    //   });
-    // }
+    }
   });
 });
 
@@ -33,41 +34,24 @@ chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
     let isInstagram = url.includes("https://www.instagram.com/");
     if (isInstagram) {
       instagramTools(tabId);
-    } 
-    // else {
-    //   chrome.action.setIcon({
-    //     path: {
-    //       "16" : "./icons/icon16.png",
-    //       "32" : "./icons/icon32.png",
-    //       "48" : "./icons/icon48.png",
-    //       "128" : "./icons/icon128.png"
-    //     },
-    //     tabId: tabId,
-    //   });
-    // }
+    }
   }
 });
 
 const instagramTools = (tabId) => {
   chrome.action.setIcon({
     path: {
-      "16" : "./icons/icon16-active.png",
-      "32" : "./icons/icon32-active.png",
-      "48" : "./icons/icon48-active.png",
-      "128" : "./icons/icon128-active.png"
+      "16": "./icons/icon16-active.png",
+      "32": "./icons/icon32-active.png",
+      "48": "./icons/icon48-active.png",
+      "128": "./icons/icon128-active.png"
     },
     tabId: tabId,
   });
-
-  chrome.contextMenus.onClicked.addListener(function (info, tab) {
-    chrome.tabs.sendMessage(
-      tab.id,
-      "getClickedEl",
-      { frameId: info.frameId },
-      (data) => {
-        let imageSrc = data.value;
-        console.log(imageSrc);
-      },
-    );
-  });
+  chrome.action.setPopup(
+    {
+      popup: "index.html",
+      tabId: tabId,
+    }
+  )
 };
